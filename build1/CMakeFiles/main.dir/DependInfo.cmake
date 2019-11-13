a9b7b77b6d889dc0b5d1690ef9aa30e191cf82a7
Kerorohu <623786029@qq.com>
2019-11-13 20:23:34 +0800

    write data to file

---
+++ build1/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/oyo/文档/src/SerialPort.cpp" "/home/oyo/文档/src/build1/CMakeFiles/main.dir/SerialPort.cpp.o"
+  "/home/oyo/文档/src/main.cpp" "/home/oyo/文档/src/build1/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
